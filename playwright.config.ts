b8165104389b470cc19411658e09d73740a0dde1
Hisham Zargar <hishamzargar@gmail.com>
2024-12-20 20:21:19 +0100

    updated report config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
         browserName: 'chromium',
         headless: false, // Set to true for headless mode
         baseURL: 'https://amazon.sa', // Set a base URL
+        trace: 'on'
     },
+    reporter: [
+        ['list'],
+        ['html', { outputFolder: 'playwright-report' }],
+      ],
 });
 
